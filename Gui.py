ebb107d6579a9a2ce6b15375b556d3eb63543fb9
bitmap357 <edakoyeram@gmail.com>
2023-07-04 16:44:16 +0000

    compression function

--- Gui.py
+++ Gui.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 from compressionmodule import compress, decompress
 
+
+def compression(i, o):
+    compress(i, o)
+
+
 window = tk.Tk()
 window.title("Compression Engine")
 window.geometry("600x400")
