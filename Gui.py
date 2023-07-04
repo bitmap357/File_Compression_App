ab8425ef8087b59de7d0a680188b3b0f6f9523db
bitmap357 <edakoyeram@gmail.com>
2023-07-04 23:02:21 +0000

    function to open local file selector

--- Gui.py
+++ Gui.py
@@ -3,6 +3,11 @@ from compressionmodule import compress, decompress
 from tkinter import filedialog
 
 
+def open_file():
+    filename = filedialog.askopenfilename(initialdir='/', title='Select a file to compress')
+    return filename
+
+
 def compression(i, o):
     compress(i, o)
 
@@ -11,16 +16,16 @@ window = tk.Tk()
 window.title("Compression Engine")
 window.geometry("600x400")
 
-input_entry = tk.Entry(window)
-output_entry = tk.Entry(window)
-
-input_label = tk.Label(window, text='File to be compressed')
-input_label.grid(row=0, column=0)
-input_entry.grid(row=0, column=1)
-
-output_label = tk.Label(window, text='Name of the compressed file')
-output_label.grid(row=0, column=0)
-output_entry.grid(row=1, column=0)
+# input_entry = tk.Entry(window)
+# output_entry = tk.Entry(window)
+#
+# input_label = tk.Label(window, text='File to be compressed')
+# input_label.grid(row=0, column=0)
+# input_entry.grid(row=0, column=1)
+#
+# output_label = tk.Label(window, text='Name of the compressed file')
+# output_label.grid(row=0, column=0)
+# output_entry.grid(row=1, column=0)
 
 compress_button = tk.Button(window, text='COMPRESS', command=lambda: compression(input_entry.get(), output_entry.get()))
 compress_button.grid(row=2, column=1)
