eb76b97348497a8fe3f39b5831b5917e5d83b4ba
bitmap357 <edakoyeram@gmail.com>
2023-07-04 17:25:03 +0000

    input and output entries

--- Gui.py
+++ Gui.py
@@ -13,4 +13,7 @@ window.geometry("600x400")
 input_entry = tk.Entry(window)
 output_entry = tk.Entry(window)
 
+input_entry.grid(row=0, column=0)
+output_entry.grid(row=1, column=0)
+
 window.mainloop()
