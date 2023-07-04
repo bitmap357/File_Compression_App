5fe6b16c7828d1ad4c79097885ec57baaaf807fd
bitmap357 <edakoyeram@gmail.com>
2023-07-04 17:30:16 +0000

    compress button

--- Gui.py
+++ Gui.py
@@ -16,8 +16,12 @@ output_entry = tk.Entry(window)
 input_label = tk.Label(window, text='File to be compressed')
 input_label.grid(row=0, column=0)
 input_entry.grid(row=0, column=1)
+
 output_label = tk.Label(window, text='Name of the compressed file')
 output_label.grid(row=0, column=0)
 output_entry.grid(row=1, column=0)
+
+compress_button = tk.Button(window, text='COMPRESS')
+compress_button.grid(row=2, column=1)
 
 window.mainloop()
