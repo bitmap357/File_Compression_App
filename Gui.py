37d57cb2ec2f4b3d93c4e3d74e3b40f9035c318f
bitmap357 <edakoyeram@gmail.com>
2023-07-04 17:28:40 +0000

    input and output label

--- Gui.py
+++ Gui.py
@@ -16,6 +16,8 @@ output_entry = tk.Entry(window)
 input_label = tk.Label(window, text='File to be compressed')
 input_label.grid(row=0, column=0)
 input_entry.grid(row=0, column=1)
+output_label = tk.Label(window, text='Name of the compressed file')
+output_label.grid(row=0, column=0)
 output_entry.grid(row=1, column=0)
 
 window.mainloop()
